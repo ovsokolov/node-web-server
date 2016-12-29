06b61ce573bba3600c28846d37ed25df1eb98bc1
Oleg Sokolov <olegsokolov@Olegs-Air.home>
2016-12-29 17:26:20 -0500

    Heroku script and port

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const port = process.env.PORT || 3000;
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintanance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/',(req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.render('home.hbs', {
+    userName: 'Oleg',
+    pageTitle: 'Home Page'
+  });
+});
+
+app.get('/about',(req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad',(req, res) => {
+  res.send({
+    errorMessage: 'Wrong Page!'
+  });
+});
+
+app.disable('etag');
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
